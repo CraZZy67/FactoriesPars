d845d89fe30c181f03b79015c6ef11114e7a876a
Sergey <128322450+CraZZy67@users.noreply.github.com>
2024-09-28 18:54:21 +0300

    Сделал обработку негативных кодов ответа

--- company_links.py
+++ company_links.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 
 import re
-from typing import Union
 
 class Companies:
 
@@ -15,7 +14,7 @@ class Companies:
         
         self.get_html()
     
-    def get_html(self) -> Union[None | str]:
+    def get_html(self) -> None:
         if self.headers != None:
             response = requests.get(url=self.link, headers=self.headers)   
         else:
@@ -34,60 +33,64 @@ class Companies:
     
     def get_steel_fabrication(self) -> None:
         for i in range(2, self.pages + 2):
-            h3_list = self.soup.find("div", class_="catalog-list").find_all("h3")
+            if self.soup != "error":
+                h3_list = self.soup.find("div", class_="catalog-list").find_all("h3")
 
-            for k in h3_list:
-                self.companies.append("https://steel-fabrication.ru" + k.find("a")["href"])
+                for k in h3_list:
+                    self.companies.append("https://steel-fabrication.ru" + k.find("a")["href"])
 
             self.link = self.link.replace(re.search(pattern=r"PAGEN_6=\d+$", string=self.link).group(0), f"PAGEN_6={i}")
-
             self.get_html()
 
     def get_metalweb(self) -> None:
         for i in range(2, self.pages + 2):
-            infinite_scroll = self.soup.find("article", class_="col-xs-12").find("div", class_="infinite-scroll")
-            div_list = infinite_scroll.find_all("div", class_="pull-left")
+            if self.soup != "error":
+                infinite_scroll = self.soup.find("article", class_="col-xs-12").find("div", class_="infinite-scroll")
+                div_list = infinite_scroll.find_all("div", class_="pull-left")
 
-            for k in div_list:
-                self.companies.append(k.find("h3").find("a")["href"])
-        
+                for k in div_list:
+                    self.companies.append(k.find("h3").find("a")["href"])
+            
             self.link = self.link.replace(re.search(pattern=r"page.*", string=self.link).group(0), f"page{i}.html")
             self.get_html()
-    
+        
     def get_fabricators(self) -> None:
         for i in range(1, self.pages + 2):
-            content_list_div = self.soup.find("div", class_="content-list--padding")
-            div_enterprise = content_list_div.find_all("div", class_="enterprise-teaser")
+            if self.soup != "error":
+                content_list_div = self.soup.find("div", class_="content-list--padding")
+                div_enterprise = content_list_div.find_all("div", class_="enterprise-teaser")
 
-            for k in div_enterprise:
-                self.companies.append("https://fabricators.ru" + k.find("a")["href"])
-            
+                for k in div_enterprise:
+                    self.companies.append("https://fabricators.ru" + k.find("a")["href"])
+                
             if i == 1:
                 self.link += "?ysclid=m1kjzaqwyj637159448&page=1"
             else:
                 self.link = self.link.replace(re.search(pattern=r"page=\d+$", string=self.link).group(0), f"page={i}")
-
             self.get_html()
+            
     
     def get_oborudunion(self) -> None:
         for i in range(2, self.pages + 2):
-            company_names = self.soup.find_all("div", class_="company-name")
+            if self.soup != "error":
+                company_names = self.soup.find_all("div", class_="company-name")
 
-            for k in company_names:
-                if k.find("a") == None:
-                    continue
-                self.companies.append("https://www.oborudunion.ru" + k.find("a")["href"])
-            
+                for k in company_names:
+                    if k.find("a") == None:
+                        continue
+                    self.companies.append("https://www.oborudunion.ru" + k.find("a")["href"])
+                
             self.link = self.link.replace(re.search(pattern=r"PAGEN_1=\d+$", string=self.link).group(0), f"PAGEN_1={i}")
             self.get_html()
     
     def get_manufacturers(self) -> None:
         for i in range(1, self.pages + 2):
-            sim_titles = self.soup.find("div", class_="companies-list").find_all("div", class_="sim-title")
+            if self.soup != "error":
+                sim_titles = self.soup.find("div", class_="companies-list").find_all("div", class_="sim-title")
 
-            for k in sim_titles:
-                self.companies.append("https://manufacturers.ru/" + k.find("a")["href"])
-            
+                for k in sim_titles:
+                    self.companies.append("https://manufacturers.ru/" + k.find("a")["href"])
+                
             if i == 1:
                 self.link += "?page=1"
             else:
@@ -96,20 +99,22 @@ class Companies:
 
     def get_orgpage(self) -> None:
         for i in range(1, self.pages + 2):
-            item_titles = self.soup.find("div", id="rubricator-result").find_all("div", class_="similar-item__title")
+            if self.soup != "error":
+                item_titles = self.soup.find("div", id="rubricator-result").find_all("div", class_="similar-item__title")
 
-            for k in item_titles:
-                self.companies.append(k.find("a")["href"])
-            
+                for k in item_titles:
+                    self.companies.append(k.find("a")["href"])
+                
             if i == 1:
                 self.link += "2/"
             else:
                 self.link = self.link.replace(re.search(pattern=r"\d+\/$", string=self.link).group(0), f"{i}/") 
             self.get_html()
-    
+            
     def get_checko(self) -> None:
         for i in range(2, self.pages + 2):
-            links = self.soup.find("table", class_="data-table").find_all("a", class_="link")
+            if self.soup != "error":
+                links = self.soup.find("table", class_="data-table").find_all("a", class_="link")
 
             for k in links:
                 self.companies.append("https://checko.ru" + k["href"])
@@ -117,13 +122,14 @@ class Companies:
             self.link = self.link.replace(re.search(pattern=r"page=\d+$", string=self.link).group(0), f"page={i}")
             self.get_html()
     
-    def get_kemcsm(self):
+    def get_kemcsm(self)-> None:
         for i in range(1, self.pages + 2):
-            factory_heads = self.soup.find("div", class_="factory-wrap").find_all("div", class_="factory__head")
+            if self.soup != "error":
+                factory_heads = self.soup.find("div", class_="factory-wrap").find_all("div", class_="factory__head")
 
-            for k in factory_heads:
-                self.companies.append("https://kemcsm.ru" + k.find("a")["href"])
-            
+                for k in factory_heads:
+                    self.companies.append("https://kemcsm.ru" + k.find("a")["href"])
+                
             if i == 1:
                 self.link += "?page=1"
             else:
@@ -132,10 +138,11 @@ class Companies:
 
     def get_wiki_prom(self) -> None:
         for i in range(2, self.pages + 2):
-            titles = self.soup.find("div", class_="list-block").find_all("div", class_="title")
+            if self.soup != "error":
+                titles = self.soup.find("div", class_="list-block").find_all("div", class_="title")
 
-            for k in titles:
-                self.companies.append(k.find("a")["href"])
+                for k in titles:
+                    self.companies.append(k.find("a")["href"])
             
             if i == 2:
                 self.link = self.link[0:28] + "/page2/" + "mashinostroitelnye-zavody.html"
